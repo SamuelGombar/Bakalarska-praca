0fdb65e1cdc37fed44e7b44683d2dae84ec692e8
Samuel Gombár <125505036+SamuelGombar@users.noreply.github.com>
2024-06-06 14:02:52 +0200

    Delete Migrate.m

--- Migrate.m
+++
@@ -1,6 +0,0 @@
-function [popTo, fitTo] = Migrate(popFrom, fitFrom, popTo, fitTo)
-    migrateBest = selbest(popFrom, fitFrom, 1);
-    [~, index] = max(fitTo);
-    popTo(index, :) = migrateBest;
-    fitTo(index) = min(fitFrom);
-end
